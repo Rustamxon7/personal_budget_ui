a852fd466f76d459959f44f2d96321c319c88738
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-02 15:14:49 +0500

    Update LogIn.js

--- src/pages/registrations/LogIn.js
+++ src/pages/registrations/LogIn.js
@@ -1,53 +1,80 @@
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+/* eslint-disable object-curly-newline */
+/* eslint-disable max-len */
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { login } from '../../redux/auth';
-import './registrations.css';
+import '../../sass/main.scss';
 
 const LogIn = () => {
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    dispatch(login(data));
-  };
 
-  const error = useSelector((state) => state.auth.error);
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required!'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required!'),
+  });
 
   return (
-    <div className="contents">
-      <div className="content--right">
-        <div className="header">
-          <h1 className="sessions--title">Personal Budget</h1>
-        </div>
-        <form action="#" method="post" className="form" onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="form--title">Welcome back</h2>
-          <p className="sub--title">Welcome back! Please enter your details.</p>
-          <div className="form--group">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" className="form--control" {...register('email', { required: true })} />
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(true);
+        dispatch(login(values));
+        setSubmitting(false);
+      }}
+      className="form"
+    >
+      {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+        <>
+          <div className="login">
+            <div className="login__content-box">
+              <div className="login__logo">
+                <img className="logo" src="img/beedget.svg" alt="Beedget logo" />
+                <span>beedget</span>
+              </div>
+              <div className="login__box">
+                <div className="login__form" action="">
+                  <h1 className="heading-primary">Welcome back!</h1>
+                  <h4 className="subheading">Please enter your details.</h4>
+                  <form onSubmit={handleSubmit}>
+                    <label className="login__label" htmlFor="email">
+                      Email
+                    </label>
+                    <input className="login__input" type="email" name="email" id="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.email && touched.email && <div className="error">{errors.email}</div>}
+                    <br />
+
+                    <label className="login__label" htmlFor="password">
+                      Password
+                    </label>
+                    <input className="login__input" type="password" name="password" id="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.password && touched.password && <div className="error">{errors.password}</div>}
+                    <br />
+                    <button type="submit" className="btn" disabled={isSubmitting}>
+                      Submit
+                    </button>
+                    <br />
+                    <br />
+                    <p className="login__text">
+                      Don&apos;t have an account?
+                      <strong>
+                        <Link to="/users/signup">Sign up</Link>
+                      </strong>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="login__image-box">&nbsp;</div>
           </div>
-          <div className="form--group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" className="form--control" {...register('password', { required: true })} />
-          </div>
-          <div className="form--group">
-            <input type="submit" value="Sign in" className="submit--btn" />
-          </div>
-          {
-            // show error if there any error else show login
-            error && <p className="error">{error}</p>
-          }
-          <p className="link-to">
-            Don&apos;t have an account?
-            {' '}
-            <Link to="/users/signup">Sign Up</Link>
-          </p>
-        </form>
-      </div>
-      <div className="content--left">
-        <img src="img/treasure.svg" alt="Success Factors" className="welcome--img" />
-      </div>
-    </div>
+        </>
+      )}
+    </Formik>
   );
 };
 
