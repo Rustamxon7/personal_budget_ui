24713962220f9b59662305076f0453d436d9a453
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-02 15:16:11 +0500

    Create lists.js

---
+++ src/lists/lists.js
@@ -0,0 +1,57 @@
+export const iconsList = [
+  { name: 'home', color: '#f44336' },
+  { name: 'airplane', color: '#e91e63' },
+  { name: 'bag-handle', color: '#9c27b0' },
+  { name: 'thunderstorm', color: '#673ab7' },
+  { name: 'storefront', color: '#3f51b5' },
+  { name: 'shirt', color: '#2196f3' },
+  { name: 'scale', color: '#03a9f4' },
+  { name: 'receipt', color: '#00bcd4' },
+  { name: 'pizza', color: '#009688' },
+  { name: 'people', color: '#4caf50' },
+  { name: 'newspaper', color: '#8bc34a' },
+  { name: 'medkit', color: '#cddc39' },
+  { name: 'library', color: '#ffeb3b' },
+  { name: 'ice-cream', color: '#ffc107' },
+  { name: 'happy', color: '#ff9800' },
+  { name: 'hammer', color: '#ff5722' },
+  { name: 'game-controller', color: '#795548' },
+  { name: 'football', color: '#607d8b' },
+  { name: 'fish', color: '#9e9e9e' },
+  { name: 'fast-food', color: '#607d8b' },
+  { name: 'desktop', color: '#9e9e9e' },
+  { name: 'construct', color: '#607d8b' },
+  { name: 'car', color: '#9e9e9e' },
+  { name: 'car-sport', color: '#607d8b' },
+  { name: 'bus', color: '#9e9e9e' },
+  { name: 'bicycle', color: '#607d8b' },
+  { name: 'basket', color: '#9e9e9e' },
+  { name: 'barbell', color: '#607d8b' },
+  { name: 'wifi', color: '#9e9e9e' },
+];
+
+export const fonds = [
+  { name: 'incomes', color: '#4caf50' },
+  { name: 'expenses', color: '#f44336' },
+];
+
+export const links = [
+  {
+    id: 1,
+    path: '/',
+    text: 'Home',
+    icon: 'home',
+  },
+  {
+    id: 2,
+    path: '/incomes',
+    text: 'Receipts',
+    icon: 'cash-outline',
+  },
+  {
+    id: 3,
+    path: '/expenses',
+    text: 'Expenses',
+    icon: 'wallet-outline',
+  },
+];
