0e4700e8a939e7d4cef36ee257f86bf906b888f3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:45:55 +0500

    Update chart.js

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,79 +1,77 @@
 /* eslint-disable max-len */
 /* eslint-disable object-curly-newline */
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import {
-  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
-} from 'recharts';
+import { AreaChart, Area, Legend, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { fetchFunds } from '../redux/funds/funds';
+import { fetchCategories } from '../redux/categories/categories';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const Chart = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-const Chart = () => (
-  <div style={{ width: '100%', height: 300 }}>
-    <ResponsiveContainer>
-      <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-          </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-      </AreaChart>
-    </ResponsiveContainer>
-  </div>
-);
+  const funds = useSelector((state) => state.funds.funds.funds);
+  const incomes = useSelector((state) => state.funds.funds.incomes);
+  const expenses = useSelector((state) => state.funds.funds.expenses);
+
+  // const categories = useSelector((state) => state.categories.categories);
+
+  useEffect(() => {
+    dispatch(fetchFunds(1));
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  const fundsLocation = (location) => {
+    if (location.pathname.split('/')[1] === 'incomes') {
+      return incomes || [];
+    }
+    if (location.pathname.split('/')[1] === 'expenses') {
+      return expenses || [];
+    }
+    if (location.pathname.split('/')[3] === 'incomes') {
+      return incomes || [];
+    }
+    if (location.pathname.split('/')[3] === 'expenses') {
+      return expenses || [];
+    }
+    return funds || [];
+  };
+
+  const data2 = fundsLocation(location).map((fund) => ({
+    name: fund.title,
+    expenses: fund.type_declaration === 'expenses' ? fund.amount : 0,
+    incomes: fund.type_declaration === 'incomes' ? fund.amount : 0,
+    amt: fund.amount,
+  }));
+
+  return (
+    <div style={{ width: '100%', height: 340 }} className="chart">
+      <ResponsiveContainer>
+        <AreaChart width={430} height={250} data={data2} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="colorincomes" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorexpenses" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Legend />
+
+          <CartesianGrid strokeDasharray="3 9" />
+          <Tooltip />
+          <Area type="monotone" dataKey="incomes" stroke="#8884d8" fillOpacity={1} fill="url(#colorincomes)" />
+          <Area type="monotone" dataKey="expenses" stroke="#82ca9d" fillOpacity={1} fill="url(#colorexpenses)" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 export default Chart;
