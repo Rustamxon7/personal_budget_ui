05646f3993fb32b2635b639ecec01163ff88e081
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:46:53 +0500

    Create Latest transactions and design

---
+++ src/components/Transactions.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Transactions = ({ data }) => {
+  const transactionsList =
+    data.length > 0 ? (
+      data.slice(0, 10).map((fund) => (
+        <div className="small-list__row" key={fund.id}>
+          <ion-icon class="small-list__icon" name={`${fund.icon}-outline`} style={{ color: fund.color }} />
+          <div className="small-list__content">
+            <span className="small-list__name">{fund.title}</span>
+            <span className="small-list__date">{fund.date}</span>
+          </div>
+          <span className="small-list__price" style={fund.type_declaration === 'expenses' ? { color: 'green' } : { color: 'red' }}>
+            {fund.type_declaration === 'expenses' ? `+ $ ${fund.amount}` : `- $ ${fund.amount}`}
+          </span>
+        </div>
+      ))
+    ) : (
+      <div className="small-list__row">
+        <ion-icon class="small-list__icon" name="cart-outline" />
+        <div className="small-list__content">
+          <span className="small-list__name">NO FUND</span>
+          <span className="small-list__date">4 Aug 10:65 PM</span>
+        </div>
+        <span className="small-list__price">0$</span>
+      </div>
+    );
+
+  return (
+    <div>
+      <div className="one-row">
+        <h3 className="heading-tertiary">Transactions</h3>
+      </div>
+      <div className="small-list">{transactionsList}</div>
+    </div>
+  );
+};
+
+Transactions.defaultProps = {
+  data: [],
+};
+
+export default Transactions;
