5e36046cac801251976566d2c3b718de40bb4402
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:46:09 +0500

    Redesigne Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
-/* eslint-disable indent */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { loadCurrentUser } from '../redux/auth';
 import PeopleList from '../pages/people/PeopleList';
 import { fetchCurrentUser } from '../redux/users/currentUser';
@@ -27,19 +25,16 @@ const Headers = () => {
 
   return (
     <header className="header">
-      <ion-icon name="search" />
+      <ion-icon class="header--icon" name="search" />
       <input type="text" placeholder="Search" />
-      <ion-icon class="notification" name="notifications-outline" />
-      {isAuthenticated.data.email ? <ion-icon class="profile--icon header__profile-icon" name="person-circle-outline" onClick={handleHover} /> : <ion-icon class="profile--icon header__profile-icon" name="person-outline" />}
+      <ion-icon class="notification header--icon" name="notifications-outline" />
+      {isAuthenticated.data.email ? <ion-icon class="profile--icon header--icon" name="person-circle-outline" onClick={handleHover} /> : <ion-icon class="profile--icon header--icon" name="person-outline" />}
       <div className="persons--menu hidden">
         <div className="persons--menu__title">
-          <span className="menu--user__name">{currentUser.name}</span>
+          <span className="menu--user__header">{currentUser.name}</span>
           <span className="user--avatar">
-            <ion-icon name="person-circle-outline" />
+            <ion-icon name="person-circle-outline" class="user--avatar__icon" />
           </span>
-          <NavLink to="/user/edit" className="edit--person">
-            <ion-icon name="create-outline" />
-          </NavLink>
         </div>
         <PeopleList />
       </div>
