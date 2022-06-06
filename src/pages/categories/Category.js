57d4ba9d52bde29df5212af7bd84a0f747a5a431
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:47:18 +0500

    Redesign Category

--- src/pages/categories/Category.js
+++ src/pages/categories/Category.js
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { fetchCategory, removeCategoryAction } from '../../redux/categories/categories';
 import { fetchFunds } from '../../redux/funds/funds';
+import EditCategory from './UpdateCategory';
 import Funds from '../funds/Funds';
 import AddFund from '../funds/AddFund';
 import Sidebar from '../../components/Sidebar';
@@ -14,8 +19,10 @@ import Header from '../../components/Header';
 const Category = () => {
   const dispatch = useDispatch();
   const location = useLocation();
+  const navigate = useNavigate();
 
   const [open, setOpen] = useState('hidden');
+  const [openEdit, setOpenEdit] = useState('disabled');
 
   const currentLocation = location.pathname.split('/')[1];
 
@@ -26,7 +33,13 @@ const Category = () => {
     return parseInt(location.pathname.split('/')[2], 10);
   };
 
+  // select by date startDate endDate
+
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
   const category = useSelector((state) => state.categories.category);
+  const loading = useSelector((state) => state.categories.loading);
 
   useEffect(() => {
     dispatch(fetchCategory(validateCurrentPerson(), location.pathname.split('/')[3]));
@@ -34,41 +47,79 @@ const Category = () => {
   }, [dispatch]);
 
   const handleRemove = () => {
-    dispatch(removeCategoryAction(location.pathname.split('/')[2]));
-    window.history.back();
+    dispatch(removeCategoryAction(validateCurrentPerson()));
+    navigate('/');
     window.location.reload();
   };
 
-  const funds = useSelector((state) => state.funds.funds);
+  const funds = useSelector((state) => state.funds.funds.funds);
+
+  const filterdFunds = funds.filter((fund) => fund.category_id === category.id) || [];
+
+  const fundsAmount = filterdFunds.reduce((acc, curr) => acc + curr.amount, 0);
+
+  const fundsCount = filterdFunds.length;
+
+  const selectByDate = () => {
+    if (startDate !== '' && endDate !== '') {
+      return funds.filter((fund) => {
+        const date = new Date(fund.date);
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        return date >= start && date <= end;
+      });
+    }
+    return funds;
+  };
 
   return (
     <div className="dashboard">
       <Sidebar />
       <main>
         <Header />
-
-        <div className="app category-page">
-          <div className="category-page__header">
-            <div className="category-page__header-left" onClick={handleRemove} onKeyPress={handleRemove} role="button" tabIndex={0}>
-              <ion-icon name="close-circle-outline" />
-            </div>
+        {loading ? (
+          <div className="app category-page loader--container">
+            <img className="loader" src="img/loader.svg" alt="Loading" />
           </div>
-          <div className="app-container">
-            <h1 className="heading-primary">{category.title} | 2560$ | 36</h1>
-            <div className="table">
-              <h3 className="table__heading">
-                <span>📆 Dec 01 - May 22, 22</span>
-                <button type="button" className="btn" onClick={() => setOpen('')}>
-                  Add
-                </button>
-              </h3>
-              <div className="table__body">
-                <Funds funds={funds} />
-                <AddFund open={open} setOpen={setOpen} />
+        ) : (
+          <>
+            <EditCategory open={openEdit} setOpen={setOpenEdit} category={category.id} />
+            <div className="app category-page">
+              <div className="category-page__header">
+                <div className="category-page__header-left" onClick={handleRemove} onKeyPress={handleRemove} role="button" tabIndex={0}>
+                  <ion-icon name="close-circle-outline" />
+                </div>
+              </div>
+              <div className="app-container">
+                <h1 className="heading-primary">
+                  {category.title} | ${fundsAmount} | {fundsCount} |{' '}
+                  <div className="category-page__link" onClick={() => setOpenEdit('')}>
+                    <ion-icon name="create-outline" />
+                  </div>{' '}
+                  |
+                  <ion-icon name="trash-outline" onClick={handleRemove} onKeyPress={handleRemove} role="button" tabIndex={0} />
+                </h1>
+                <div className="table">
+                  <h3 className="table__heading">
+                    <span>
+                      <ion-icon name="calendar-outline" onClick={selectByDate} onKeyPress={selectByDate} role="button" tabIndex={0} />
+                      <input type="date" onChange={(e) => setStartDate(e.target.value)} />
+                      -
+                      <input type="date" onChange={(e) => setEndDate(e.target.value)} />
+                    </span>
+                    <button type="button" className="btn" onClick={() => setOpen('')}>
+                      Add
+                    </button>
+                  </h3>
+                  <div className="table__body">
+                    <Funds funds={filterdFunds} category={category} startDate={startDate} endDate={endDate} />
+                    <AddFund open={open} setOpen={setOpen} category={category} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </main>
     </div>
   );
